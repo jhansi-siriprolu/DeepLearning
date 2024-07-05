37368521cafb99cb463d3a67738086b4392ac712
Jhansi <siriprolu2018@gmail.com>
2024-07-05 19:03:21 +0530

    dataset folder is added to .gitignore file

---
+++ DL Projects/MultiClassClassificationModel/model.py
@@ -0,0 +1,8 @@
+#HospitalHabit - MultiClassification Problem.
+## 11 different classes from 0-10 days unto 100+ days
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("../dataset/")
+df.head()
